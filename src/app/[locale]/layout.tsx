7ecf52b545b7a777c8ccdd808e8b232404e9786f
josh-kennedy <archeryrangescanada@gmail.com>
2026-03-10 14:50:57 -0400

    fix: Correct next-intl import paths and navigation export for build

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import Script from 'next/script'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
-import './globals.css'
+import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
